e6e37ecdb7725d09d381731a29749c2b8b26164f
Lucas <lweel@mbovmm.nl>
2019-10-08 11:34:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UberCoolTicTacToe5
+{
+    class Game
+    {
+        public bool gameEnd = true;
+        List<char> board = new List<char>();
+        char playerA = 'X';
+        char playerB = 'O';
+        enum State { TurnA, TurnB };
+        State state = State.TurnA;
+
+
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Init();
+            while (!game.gameEnd)
+            {
+                game.ShowBoard();
+                game.GetMove();
+                game.CheckForWinner();
+            }
+
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+
+        void Init()
+        {
+        }
+
+        void ShowBoard()
+        {
+        }
+
+        void GetMove()
+        {
+        }
+
+        void CheckForWinner()
+        {
+        }
+    }
+}
